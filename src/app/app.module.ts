46ec284a288baa8394145927ae548abe999a18ed
Diego Santos <santos.diegoferreira@gmail.com>
2022-06-25 09:55:55 -0300

    refactor: Adiciona comentários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,17 +22,18 @@ import { CapaComponent } from './capa/capa.component';
   imports: [
     BrowserModule,
     RouterModule.forRoot([
+      /* Controla as rotas que devem ser utilizadas para exibir os conteúdos */
       { path: 'capa', component: CapaComponent },
       { path: 'sobre', component: SobreComponent },
       { path: 'formacao', component: FormacaoComponent },
       { path: 'portfolio', component: PortfolioComponent },
       { path: 'contato', component: ContatoComponent },
-      {
+      { /* Essa é a rota padrão quando nenuma página é selecionada */
         path: '',
         redirectTo: '/capa',
         pathMatch: 'full'
       },
-      {
+      { /* Essa rota força que a capa seja exibida quando alguma URL inválida é digitada */
         path: '**',
         redirectTo: '/capa',
       },
